35673cabc68fb4434b0c57973b59cc808d93b3c1
yewl1110 <yewl1110@naver.com>
2019-10-10 00:14:16 +0900

    permission 수정

--- app/src/main/java/com/example/fish/MainActivity.java
+++ app/src/main/java/com/example/fish/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.fish;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -40,24 +42,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //DB에 접근하기위해 (내부저장소) 권한설정
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-        ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE
-            )
-            ) {
-
-            } else {
-                ActivityCompat.requestPermissions(this, new String[] {"Manifest.permission.READ_EXTERNAL_STORAGE"}, 1);
-            }
+        if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)){
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setTitle("권한이 필요합니다.")
+                  .setMessage("외부 저장소 읽기 권한을 허용합니다.")
+                  .setPositiveButton("네", new DialogInterface.OnClickListener() {
+                      @Override
+                      public void onClick(DialogInterface dialogInterface, int i) {
+                          ActivityCompat.requestPermissions(MainActivity.this, new String[] {"Manifest.permission.READ_EXTERNAL_STORAGE"}, 1);
+                      }
+                  })
+                  .setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                      @Override
+                      public void onClick(DialogInterface dialogInterface, int i) {
+                          finish();
+                      }
+                  })
+                  .create()
+                  .show();
         }
 
+
         //get serial number
-        if (ContextCompat.checkSelfPermission(
+        if(shouldShowRequestPermissionRationale(Manifest.permission.READ_PHONE_STATE)){
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setTitle("권한이 필요합니다.")
+                    .setMessage("외부 저장소 읽기 권한을 허용합니다.")
+                    .setPositiveButton("네", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[] {"Manifest.permission.READ_PHONE_STATE"}, 1);
+                        }
+                    })
+                    .setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            finish();
+                        }
+                    })
+                    .create()
+                    .show();
+        }
+
+        /*if (ContextCompat.checkSelfPermission(
                 this,
                 Manifest.permission.READ_PHONE_STATE
         ) != PackageManager.PERMISSION_GRANTED
@@ -68,6 +95,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 ActivityCompat.requestPermissions(this, new String[] {"Manifest.permission.READ_PHONE_STATE"}, 1);
             }
         }
+         */
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
